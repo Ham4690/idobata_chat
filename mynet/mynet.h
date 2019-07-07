effcbd0a5ed853c6d7876781a9be625194c03f13
Ham4690 <ham46903@gmail.com>
2019-07-07 09:45:13 +0900

    mynet.h add prototype declaration

--- mynet/mynet.h
+++ mynet/mynet.h
@@ -13,9 +13,26 @@
 #include <netdb.h>
 #include <unistd.h>
 
+//init_tcpserver.c
 int init_tcpserver(in_port_t myport, int backlog);
+//init_tcpclient.c
 int init_tcpclient(char *servername, in_port_t serverport);
+//init_udpserver.c
+int init_udpserver(in_port_t myport);
+//init_udpclient.c
+int init_udpclient();
+void set_sockaddr_in(struct sockaddr_in *server_adrs, 
+		     char *servername, in_port_t port_number );
+void set_sockaddr_in_broadcast(struct sockaddr_in *server_adrs, 
+		               in_port_t port_number );
+//other.c      
 void exit_errmesg(char *errmesg);
+int Accept(int s, struct sockaddr *addr, socklen_t *addrlen);
+int Send(int s, void *buf, size_t len, int flags);
+int Recv(int s, void *buf, size_t len, int flags);
+int Sendto( int sock, const void *s_buf, size_t strsize, int flags, const struct sockaddr *to, socklen_t tolen);
+int Recvfrom(int sock, void *r_buf, size_t len, int flags,
+       struct sockaddr *from, socklen_t *fromlen);
 int analyze_header( char *header );
 void create_packet(int type, char *message );
 
